341878d1159a6dd8e261acf20957e4fa324864d0
sasham43 <sashasemail@gmail.com>
2018-01-22 17:46:24 -0600

    mostly

--- data.js
+++ data.js
@@ -1,7 +1,10 @@
-var fs = require('fs');
+// var fs = require('fs');
+var fs = require('graceful-fs');
 var path = require('path');
 
 var collection_path = '../collection/objects';
+
+var works = [];
 
 fs.readdir(collection_path, function(err, items){
     if(err)
@@ -10,7 +13,17 @@ fs.readdir(collection_path, function(err, items){
     // console.log('items:', items);
     items.forEach(function(folder){
         fs.readdir(collection_path + '/' + folder, function(err, pieces){
-            console.log('pieces:', pieces);
+            // console.log('pieces:', pieces);
+            pieces.forEach(function(piece){
+                fs.readFile(collection_path + '/' + folder + '/' + piece, 'utf-8', function(err, data){
+                    if(err)
+                        console.log('piece err', err);
+
+                    // console.log('piece:', data)
+                    works.push(JSON.parse(data));
+                    console.log('works', works.length);
+                });
+            });
         });
     });
 });
